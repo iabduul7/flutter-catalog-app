22a8d5e2ea2a842454994bfe8edb0c92a8321740
Abdullah Shahneel <abdullahshahneel@outlook.com>
2021-10-31 17:36:47 +0500

    minor fixes

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -11,84 +11,116 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   String name = '';
   bool changedButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changedButton = true;
+      });
+      await Future.delayed(const Duration(seconds: 1));
+      await Navigator.pushNamed(context, Routes.home);
+      setState(() {
+        changedButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(children: [
-        Image.asset(
-          'assets/images/login.png',
-          fit: BoxFit.cover,
-        ),
-        const SizedBox(
-          height: 18.0,
-        ),
-        const Text(
-          'Welcome to Flutter',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                    hintText: 'Enter your email', labelText: 'Email'),
-                onChanged: (value) {
-                  name = value;
-                  setState(() {});
-                },
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                    hintText: 'Enter your password', labelText: 'Password'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            minWidth: 200.0,
+            minHeight: 200.0,
+          ),
+          child: Material(
+            child: Column(children: [
+              Image.asset(
+                'assets/images/login.png',
+                fit: BoxFit.cover,
               ),
               const SizedBox(
-                height: 40.0,
+                height: 18.0,
               ),
-              InkWell(
-                onTap: () async {
-                  setState(() {
-                    changedButton = true;
-                  });
-                  await Future.delayed(const Duration(seconds: 1));
-                  // Navigator.pushNamed(context, Routes.home);
-                },
-                child: AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  width: changedButton ? 50 : 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  child: changedButton
-                      ? const Icon(
-                          Icons.done,
-                          color: Colors.white,
-                        )
-                      : const Text(
-                          'Login',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
+              const Text(
+                'Welcome to Flutter',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Enter your email', labelText: 'Email'),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                        validator: (value) {
+                          return value!.isEmpty
+                              ? 'Email field is required'
+                              : null;
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            hintText: 'Enter your password',
+                            labelText: 'Password'),
+                        validator: (value) {
+                          return value!.isEmpty
+                              ? 'Password field is required'
+                              : (value.length < 6
+                                  ? 'Password must be at least 6 characters'
+                                  : null);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      Material(
+                        color: changedButton ? Colors.green : Colors.blue,
+                        borderRadius:
+                            BorderRadius.circular(changedButton ? 50 : 8),
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 1),
+                            width: changedButton ? 50 : 150,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: changedButton
+                                ? const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                          ),
                         ),
-                  decoration: BoxDecoration(
-                      color: changedButton ? Colors.green : Colors.blue,
-                      borderRadius:
-                          BorderRadius.circular(changedButton ? 50 : 8)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              // ElevatedButton(
-              //   onPressed: () {
-              //     Navigator.pushNamed(context, Routes.home);
-              //   },
-              //   child: const Text('Login'),
-              //   style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
-              // )
-            ],
+            ]),
+            color: Colors.white,
           ),
         ),
-      ]),
-      color: Colors.white,
+      ),
     );
   }
 }
